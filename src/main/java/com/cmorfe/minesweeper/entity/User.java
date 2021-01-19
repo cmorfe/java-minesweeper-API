186f3c646dc481abdfb033e8f67e53b75a6e3d18
cmorfe <cmorfe@alquilando.com>
2021-01-19 14:25:28 -0300

    user model

---
+++ src/main/java/com/cmorfe/minesweeper/entity/User.java
@@ -0,0 +1,43 @@
+package com.cmorfe.minesweeper.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
